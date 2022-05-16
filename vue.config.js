7a8a70e05bd79bfa8a05e3a59342479c85c160d0
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-05-16 21:15:28 +0800

    change route path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/Jotes_FE/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Jotes/" : "/",
 });
